7b815bd16fa087e28b97c4f3f6999ce348a76dcb
Chaz KeyArx <chaz@keyarx.com>
2026-03-19 17:08:30 +0000

    fix: restore Spline native scroll events for capsule pop-open animation

--- components/SplineHero.tsx
+++ components/SplineHero.tsx
@@ -1,26 +1,11 @@
 "use client";
 import Spline from "@splinetool/react-spline";
-import { useScroll, useTransform, motion } from "framer-motion";
-import { useRef } from "react";
 
 export default function SplineHero() {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
-
-  // Capsules drift upward as you scroll down (parallax)
-  const y = useTransform(scrollYProgress, [0, 1], ["0%", "-25%"]);
-  const opacity = useTransform(scrollYProgress, [0, 0.8], [1, 0]);
-
   return (
-    <motion.div
-      ref={ref}
+    <div
       className="absolute inset-0"
       style={{
-        y,
-        opacity,
         filter: "hue-rotate(-30deg) saturate(0.65) brightness(0.82)",
       }}
     >
@@ -28,6 +13,6 @@ export default function SplineHero() {
         scene="https://prod.spline.design/Qb2AmcNrCEdOhDdc/scene.splinecode"
         style={{ width: "100%", height: "100%" }}
       />
-    </motion.div>
+    </div>
   );
 }
